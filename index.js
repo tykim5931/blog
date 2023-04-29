da5a14b6ee6cf402204c095490ff05b4f0297342
tykim5931 <tykim5931@gmail.com>
2023-04-29 17:39:44 +0900

    wip

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function openTab(tabName) {
-    var i;
-    var x = document.getElementsByClassName("tab-content");
-    var nav_items = document.getElementsByClassName("nav-item");
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-      if(x[i].id == tabName) {
-        nav_items[i].className +=' active';
-      } else {
-        nav_items[i].className = 'nav-item';
-      }
+  var i;
+  var x = document.getElementsByClassName("tab-pane");
+  var nav_items = document.getElementsByClassName("nav-item");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    if (x[i].id == tabName) {
+      nav_items[i].className += " active";
+    } else {
+      nav_items[i].className = "nav-item";
     }
-    document.getElementById(tabName).style.display = "block";
   }
+  document.getElementById(tabName).style.display = "block";
+}
